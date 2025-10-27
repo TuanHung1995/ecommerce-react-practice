7dfb8e052d461b036ed8e597d1693f973b780064
Tuan Hung Nguyen <tuanhungg1995@gmail.com>
2025-10-27 13:56:15 +0700

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { Routes, Route } from 'react-router'
+
 import HomePage from './pages/HomePage.jsx'
+import CheckoutPage from './pages/CheckoutPage.jsx'
+import OrderPage from './pages/OrderPage.jsx'
 import './App.css'
+import TrackingPage from './pages/TrackingPage.jsx'
 
 function App() {
 
   return (
-      <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrderPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+    </Routes>
   )
 }
 
